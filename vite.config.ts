9b2df1c77538242e28e4a0d5c0cbc31decef3e61
m-naim <mehenaouin@gmail.com>
2026-06-09 00:35:50 +0200

    Create static documentation page at /docs/index.html and link to it from the landing page header

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         app: resolve(__dirname, 'app/index.html'),
+        docs: resolve(__dirname, 'docs/index.html'),
       }
     }
   }
